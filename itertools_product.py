64a946a4c2269550338d8db8ba28f3bc212c0932
Ana Kokan <anaxkokan@gmail.com>
2021-04-26 12:36:21 +0100

    Improve style

--- itertools_product.py
+++ itertools_product.py
@@ -8,7 +8,7 @@ b = (input())
 b_list = (list((b).split()))
 int_a = [int(i) for i in a_list]
 int_b = [int(i) for i in b_list]
-x = list(product(int_a, int_b))
+cart_product_list = list(product(int_a, int_b))
 
-b = " ".join([str(i) for i in x])
-print(b)
+cart_product = " ".join([str(i) for i in cart_product_list])
+print(cart_product)
